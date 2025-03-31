bf3510898b95fd0bf510344cb9268a546257da0a
Hack-2wo <johnrikmangubar@gmail.com>
2025-03-31 23:55:28 +0800

    Delete FINAL_PROJECT directory

--- FINAL_PROJECT/PROFILE_WEB/ProfileWebstore.js
+++
@@ -1,9 +0,0 @@
-function showSidebar() {
-    const sidebar = document.querySelector('#sideBar')
-    sidebar.style.display = 'flex'
-}
-
-function hideSidebar() {
-    const sidebar = document.querySelector('#sideBar')
-    sidebar.style.display = 'none'
-}
